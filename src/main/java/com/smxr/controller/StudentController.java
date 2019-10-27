b24a6686b79dc3fcccff53e11aef0e726fbb70da
FavorIceCreamFeel <772519606@qq.com>
2019-10-27 20:45:57 +0800

    添加日志和环境切换

--- src/main/java/com/smxr/controller/StudentController.java
+++ src/main/java/com/smxr/controller/StudentController.java
@@ -3,6 +3,8 @@ package com.smxr.controller;
 import com.github.pagehelper.PageInfo;
 import com.smxr.pojo.Student;
 import com.smxr.service.StudentService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
@@ -21,20 +23,26 @@ import java.util.HashMap;
 @Controller
 @RequestMapping("/Home")
 public class StudentController {
+    private static Logger logger=LoggerFactory.getLogger(StudentController.class);
     @Autowired
     private StudentService studentService;
     @RequestMapping("/list")
     public String studentController2(@RequestParam(name = "pageNum",required = false,defaultValue = "1")Integer pageNum,
                                          @RequestParam(name = "pageSize",required = false,defaultValue = "5") Integer pageSize ,
                                      Model model){
+        logger.info("分页请求到达：=========>>>>>>>>>>");
+        logger.info("当前页数："+pageNum+"页面数量："+pageSize);
 //        用户分页
         PageInfo<Student> studentPageInfo = studentService.queryStudent(pageNum, pageSize);
+        logger.info("页面参数："+studentPageInfo);
         model.addAttribute("studentPageInfo",studentPageInfo);
 //        获取用户名字并传入页面
         String name = SecurityContextHolder.getContext().getAuthentication().getName();
+        logger.info("获取用户名："+name);
         HashMap<String, String> hashMap = new HashMap<>();
         hashMap.put("userName",name);
         model.addAttribute(hashMap);
+        logger.info("请求完成返回页面模板========>>>>>>>>>");
         return "showTable";
     }
 
